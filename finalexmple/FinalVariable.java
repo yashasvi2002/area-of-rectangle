9ac037dd8ce9abbbb86f7de6e5055b69046784e3
yashasvi2002 <143791236+yashasvi2002@users.noreply.github.com>
2024-06-09 23:13:33 +0530

    Add files via upload

---
+++ finalexmple/FinalVariable.java
@@ -0,0 +1,15 @@
+package finalexmple;
+
+public class FinalVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int salary;
+		
+		public FinalVariable()
+		{
+			salary=89;
+		}
+	}
+
+}
