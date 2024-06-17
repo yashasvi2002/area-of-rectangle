576a10ab125fbbc7b3d307d7f31fcadb17fc81b3
yashasvi2002 <143791236+yashasvi2002@users.noreply.github.com>
2024-06-17 13:19:14 +0530

    Add files via upload

---
+++ constructorEx/DisplayInfo.java
@@ -0,0 +1,12 @@
+package constructorEx;
+
+public class DisplayInfo {
+	public static void main(String[] args) {
+		employeeInfo emp = new employeeInfo("roshani","roshani12@gmail.com",909798);
+		
+		
+		System.out.println(emp);
+		
+		
+	}
+}
